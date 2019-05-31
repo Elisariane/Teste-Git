0177a754be6eb68028992538df3bd1a7a717c3b6
Elisariane <elisarianeBS@gmail.com>
2019-05-31 20:23:04 -0300

    Coloquei ponteiros e adicionei um readme

--- hello.c
+++ hello.c
@@ -5,7 +5,21 @@
 
 int main() //Função obrigatoria
 {
-    printf("Olá mundo\n");
+    int idade, *p;
+    idade = 18;
+    
+    p = &idade;
+        
+    *p = 22;
+    
+    idade = (int)p;
+    
+
+	printf("Olá mundo, a idade é %i\n", idade);
+	idade = 31;
+    printf("Olá mundo, a idade é %i\n", *p);
+
+    printf("O endereço da variavel idade é: %p\n",p);
 
     return 0; 
 
