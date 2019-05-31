78e2e60e02796f7e0cc0b1f23ec9177970509364
Elisariane <elisarianeBS@gmail.com>
2019-05-31 19:58:36 -0300

    Primeiro códigoem C

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h> //Importando uma biblioteca de entrada e saída padrão no C 
+
+//No C tudo é obrigado estar em uma função
+
+
+int main() //Função obrigatoria
+{
+    printf("Olá mundo\n");
+
+    return 0; 
+
+}
